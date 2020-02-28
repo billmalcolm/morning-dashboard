0800c6245d70dc3171d3e0009a05c5ae20d87883
Bill Malcolm <bmalcolm@gmrmarketing.com>
2020-02-28 15:31:10 -0600

    Setting up Fetch for current weather

--- src/Containers/weather.js
+++ src/Containers/weather.js
@@ -4,20 +4,57 @@ class Weather extends React.Component {
     constructor(props) {
         super(props);
 
-        this.state = {}
+        this.state = {
+            currentTemp: '',
+            currentSky: '',
+
+        }
     }
+
+    // openweather : "http://api.openweathermap.org/data/2.5/weather?lat=42.9723&lon=-88.0037&appid=5d15af8ebf8d890b4d0bca7b1433040d"
+    // current weather: http://api.openweathermap.org/data/2.5/weather?lat=42.9723&lon=-88.0037&appid=5d15af8ebf8d890b4d0bca7b1433040d
+    // forecast: http://api.openweathermap.org/data/2.5/forecast/?lat=42.9723&lon=-88.0037&APPID=5d15af8ebf8d890b4d0bca7b1433040d
+
+
+    componentWillMount() {
+        fetch('http://api.openweathermap.org/data/2.5/weather?lat=42.9723&lon=-88.0037&appid=5d15af8ebf8d890b4d0bca7b1433040d')
+            .then(results => {
+                return results.json();
+            }).then(data => {
+                let currently = data;
+                this.setState({
+                    currentTemp: currently.main.temp,
+                    currentSky: currently.weather[0].description
+                });
+
+            })
+    }
+
+
+
 
 
     render() {
+
+
+        let nowK = this.state.currentTemp;
+        function convertKtoF(k) {
+            let p1 = k - 273.15;
+            let p2 = p1 * 9 / 5;
+            let f = p2 + 32;
+
+            return Math.round(f)
+        }
+
+
+        let currentF = convertKtoF(this.state.currentTemp)
+
         return (
             <div>
-
+                Currently:{currentF}&deg;F, {this.state.currentSky}
             </div>
         );
-
-
     }
-
 }
 
 export default Weather;
